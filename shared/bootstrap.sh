10267c90c631bf573351c949aaa9650262b23337
Michael Rubinsky <mrubinsk@theupstairsroom.com>
2015-06-29 15:04:30 -0400

    Setup dovecot and mysql properly. Copy default *.conf files.

--- shared/bootstrap.sh
+++ shared/bootstrap.sh
@@ -46,15 +46,57 @@ apt-get upgrade -y
 #	--without-mysql
 #make install
 
-# mysql
-debconf-set-selections <<< 'mysql-server mysql-server/root_password password password'
-debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password password'
-apt-get install -y mysql-server
+# mysql password
+PASSWORD='password'
+HORDEDIR='/var/www/html/horde'
 
-mysql -u root --password=password -e "create database horde";
+# mysql
+debconf-set-selections <<< "mysql-server mysql-server/root_password password $PASSWORD"
+debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $PASSWORD"
+apt-get -y install mysql-server-5.5
+
+mysql -u root --password=$PASSWORD -e "create database horde";
+
+echo 'Provisioning Environment with Dovecot';
+if which dovecot > /dev/null; then
+    echo 'Dovecot is already installed'
+else
+    echo 'Installing Dovecot'
+    sudo apt-get -qq -y install dovecot-imapd 
+    sudo touch /etc/dovecot/local.conf
+    sudo echo 'mail_location = maildir:/home/%u/Maildir' >> /etc/dovecot/local.conf
+    sudo echo 'disable_plaintext_auth = no' >> /etc/dovecot/local.conf
+    sudo restart dovecot
+fi
+
+# Generate a test user.
+if getent passwd testuser > /dev/null; then
+    echo 'testuser already exists'
+else
+    echo 'Creating User "testuser" with password "password"'
+    sudo useradd testuser -m -s /bin/bash
+    echo "testuser:password"|sudo chpasswd
+    echo 'User created'
+ fi
 
 # Add PHP5/Apache from repo
-apt-get install -y php5 php5-dev php-pear php5-mysql phpunit
+apt-get -y install apache2
+apt-get -y install php5 php5-dev php-pear php5-mysql phpunit
+# setup hosts file
+#VHOST=$(cat <<EOF
+#<VirtualHost *:80>
+#    DocumentRoot "/var/www/html/${HORDEDIR}"
+#    <Directory "/var/www/html/${HORDEDIR}">
+#        AllowOverride All
+#        Require all granted
+#    </Directory>
+#</VirtualHost>
+#EOF
+#)
+#echo "${VHOST}" > /etc/apache2/sites-available/000-default.conf
+
+# enable mod_rewrite
+sudo a2enmod rewrite
 
 # Add php-ini location
 pear config-set php_ini /etc/php5/apache2/php.ini
