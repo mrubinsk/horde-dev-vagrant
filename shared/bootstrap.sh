b273cfa909a4f03878200cea03c46827f75b34f6
Michael Rubinsky <mrubinsk@theupstairsroom.com>
2015-07-19 17:40:40 -0400

    Add postfix provisioning.
    Adds postfix for LOCAL delivery only. All localusers can
    send mail to each other i.e. testuser@localhost

--- shared/bootstrap.sh
+++ shared/bootstrap.sh
@@ -46,6 +46,18 @@ else
     echo 'User created'
 fi
 
+# TODO, make this configurable.
+echo 'Provisioning Environment with Postfix for LOCAL delivery ONLY.';
+if which postfix > /dev/null; then
+    echo 'Postfix is already installed.'
+else
+    debconf-set-selections <<< "postfix postfix/mailname string $HOSTNAME"
+    debconf-set-selections <<< "postfix postfix/main_mailer_type string Local only"
+    sudo apt-get install -y postfix
+    service postfix reload
+    echo -e 'home_mailbox = mail/inbox' | sudo tee -a /etc/postfix/main.cf
+fi
+
 # Generate the admin user.
 if getent passwd $ADMINUSER > /dev/null; then
     echo "$ADMINUSER already exists"
