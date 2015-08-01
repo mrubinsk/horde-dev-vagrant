09fc87374bc19e88de05f892d4d86b6847a8e9af
Michael J Rubinsky <mrubinsk@horde.org>
2015-08-01 14:25:13 -0400

    Add prefix

--- setup/git-master-dev/ingo-procmail-backends.local.php
+++ setup/git-master-dev/ingo-procmail-backends.local.php
@@ -1,3 +1,4 @@
 <?php
 $backends['imap']['disabled'] = true;
 $backends['procmail']['disabled'] = false;
+$backends['procmail']['transport'][Ingo::RULE_ALL]['params']['delivery_mailbox_prefix'] = 'mail/';
