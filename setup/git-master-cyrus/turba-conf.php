fd09fe9d3b1fcafc03259b84b1cc0d4366323363
Michael J Rubinsky <mrubinsk@horde.org>
2016-02-09 12:10:08 -0500

    Fix shares config for turba/kolab.

--- setup/git-master-cyrus/turba-conf.php
+++ setup/git-master-cyrus/turba-conf.php
@@ -2,7 +2,7 @@
 /* CONFIG START. DO NOT CHANGE ANYTHING IN OR AFTER THIS LINE. */
 // $Id: 81b6909679bfb16a77eafefd50ce11f5d08cdd6a $
 $conf['menu']['import_export'] = true;
-$conf['shares']['source'] = 'localsql';
+$conf['shares']['source'] = 'kolab';
 $conf['comments']['allow'] = true;
 $conf['documents']['type'] = 'horde';
 $conf['tags']['enabled'] = true;
