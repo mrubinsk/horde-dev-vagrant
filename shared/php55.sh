5a67c5bbccffc717895c8974d9f3144d0ff32c9c
Michael J Rubinsky <mrubinsk@horde.org>
2016-05-17 09:57:17 -0400

    Add php56 image.

--- shared/php55.sh
+++ shared/php55.sh
@@ -6,7 +6,7 @@ echo "Provisioning for PHP 5.5.x"
 add-apt-repository -y ppa:ondrej/php5
 apt-get update
 
-apt-get -y install php5 php5-dev php-pear php5-mysql php5-intl php5-tidy php5-mcrypt php5-gd
+apt-get -y install php5 php5-dev php-pear php5-mysql php5-intl php5-tidy php5-mcrypt php5-gd php5-imagick
 
 echo "Upgrading PEAR"
 pear channel-update pear
--- shared/php56.sh
+++ shared/php56.sh
@@ -6,13 +6,14 @@ echo "Provisioning for PHP 5.6.x"
 add-apt-repository -y ppa:ondrej/php5-5.6
 apt-get update
 
-apt-get -y install php5 php5-dev php-pear php5-mysql php5-intl php5-tidy php5-mcrypt php5-gd php5-imagick phpunit
+apt-get -y install php5 php5-dev php-pear php5-mysql php5-intl php5-tidy php5-mcrypt php5-gd php5-imagick
 
 echo "Upgrading PEAR"
 pear channel-update pear
 pear upgrade -c pear
 
 pear install Math_BigInteger
+pecl install jsonc
 
 # enable mod_rewrite
 a2enmod rewrite
@@ -20,6 +21,8 @@ a2enmod rewrite
 echo "Adding Alias rule for ActiveSync"
 sudo mv /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/000-default.conf.bak
 sudo awk '/<VirtualHost/ { print; print "Alias /Microsoft-Server-ActiveSync /var/www/html/horde/rpc.php"; next}1' /etc/apache2/sites-available/000-default.conf.bak > /etc/apache2/sites-available/000-default.conf
+sudo mv /etc/apache2/sites-available/default-ssl.conf /etc/apache2/sites-available/default-ssl.conf.bak
+sudo awk '/<VirtualHost/ { print; print "Alias /Microsoft-Server-ActiveSync /var/www/html/horde/rpc.php"; next}1' /etc/apache2/sites-available/default-ssl.conf.bak > /etc/apache2/sites-available/default-ssl.conf
 
 # Add php-ini location
 pear config-set php_ini /etc/php5/apache2/php.ini
