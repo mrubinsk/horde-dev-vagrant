5a45bf99646150965b2fb01d9cdad01b79ed0a13
Michael J Rubinsky <mrubinsk@horde.org>
2017-05-11 02:11:03 -0400

    That's too many hours I'll never get back....
    Seems like the zip archive of Archive_Tar that composer downloads
    if the zip extension is present is broken. We get the old 'lone zero
    block' error from ages ago. Don't include the php-zip package
    for now to avoid this error.

--- shared/php/php7.sh
+++ shared/php/php7.sh
@@ -6,4 +6,4 @@ echo "Provisioning for PHP 7.0"
 add-apt-repository ppa:ondrej/php
 apt-get update
 
-apt-get -y install php php-dev php-mysql php-gd php-imagick php-mbstring php-zip
+apt-get -y install php php-dev php-mysql php-gd php-imagick php-mbstring
