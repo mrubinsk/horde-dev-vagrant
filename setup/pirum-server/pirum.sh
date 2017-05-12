76e87f364c135a122f682d41fdbab015c8347245
Michael J Rubinsky <mrubinsk@horde.org>
2017-05-11 23:03:03 -0400

    Make vagrant box more compatible with Scaleway, and fix testpear url.

--- setup/pirum-server/pirum.sh
+++ setup/pirum-server/pirum.sh
@@ -5,11 +5,13 @@ apt-get install -y git
 #mkdir -p /var/www/pear.horde.org
 cp /vagrant/pirum.xml /var/www/pirum.xml
 mkdir -p /horde/src
-chown vagrant /horde/src/
 
 echo "Building PIRUM channel"
-su - vagrant -c "git clone https://github.com/horde/Pirum.git /horde/src/Pirum; cd /horde/src/Pirum; git checkout production"
+git clone https://github.com/horde/Pirum.git /horde/src/Pirum; cd /horde/src/Pirum; git checkout production
 cp /horde/src/Pirum/pirum /usr/local/bin/pirum
 php /usr/local/bin/pirum build /var/www/
+chown -R demo:users /var/www
+usermod -G users demo
 
+# Cleanup
 sudo apt-get -y autoremove
